f49f2b5235b85c7b2ef839e236fdc3bdc18936d1
derkach2288 <derkach2288@gmail.com>
2022-07-22 11:47:59 +0300

    add comment

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,26 +1,38 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: %i[update show destroy edit]
+  before_action :set_question, only: %i[update show destroy edit]   # before_action викликає метод set_question із приватних методів перед методами []
 
   def create
-    @question = Question.create(question_params)
+    @question = Question.create(question_params) 
+    
+                                      # @question = Question.create(body: params[:question][:body], 
+                                      # user_id: params[:question][:user_id])
+                                      # або
+                                      # @question = Question.create(params[:questions]) 
 
-    redirect_to question_path(@question), notice: 'Нове питання створено'
+    redirect_to question_path(@question), notice: 'Нове питання створено'    # notice: це флеш, далі у layout
   end
 
   def update
     @question.update(question_params)
+                                         # @question = Question.find(params[id:])
+                                         # @question.update(
+                                            # body: params[:question][:body], 
+                                            # user_id: params[:question][:user_id]
+                                            # )
 
     redirect_to question_path(@question), notice: 'Запитання збережено'
   end
 
   def destroy
     @question.destroy
+                                        # @question = Question.find(params[id:])
+                                        # @question.destroy
 
     redirect_to questions_path, notice: 'Запитання видалено'
   end
 
   def  show
-
+                                        # @question = Question.find(params[id:])
   end
 
   def index
@@ -33,6 +45,7 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+                                      # @question = Question.find(params[id:])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'questions#index'
+  root to: 'questions#index'  # щоб не писати на головній сторінці шлях /questions
   resources:questions
+                                    # post '/questions', to: 'questions#create'
+                                    # patch '/questions/:id', to: 'questions#update'
+                                    # delete '/questions/:id', to: 'questions#destroy'
 end
